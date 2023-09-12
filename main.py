60827365373120e5cd73c57cdaa496b544c746f5
USER <vosleytee@gmail.com>
2023-09-12 15:33:56 +0100

    'main.py'

--- main.py
+++ main.py
@@ -3,3 +3,17 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+def main():
+    book = 'Dracula'
+    author = 'Bram Stoker'
+    release_year = 1987
+    goodreads_rating = 4.01
+
+    print(book)
+    print(author)
+    print(release_year)
+    print(goodreads_rating)
+
+if __name__ == '__main__':
+    main()
