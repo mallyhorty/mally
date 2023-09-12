4f078466038938fe92adc658c854ec3e08f19453
USER <vosleytee@gmail.com>
2023-09-12 16:13:57 +0100

    'main.py'

--- main.py
+++ main.py
@@ -48,3 +48,20 @@ def main():
 if __name__ == '__main__':
     main()
 
+    def main():
+        book, author, release_year, goodreads_rating = 'Dracula', 'Bram Stoker', 1987, 4.01
+
+        print(book + ' is a novel by ' + author + ', published in ' + str(release_year) + ' .It has a rating of  ' + str(goodreads_rating) + ' on goodreads. ')
+
+if __name__ == '__main__':
+    main()
+
+def main():
+    platform, pilot_name, flight_hour, Tot, Eta = 'A-29', 'Flt lt Ahmadu II', 500, 09.00, 12.30
+    print(platform + ' is a fighter jet flew by ' + pilot_name + ', with total of flying hours of ' + str(flight_hour) + ' Take off time is ' + str(Tot) + ' Estimated time of Arrival is ' + str(Eta) + ' on flight. ' )
+
+
+if __name__ == '__main__':
+    main()
+
+
